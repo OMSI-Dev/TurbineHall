b7ca1b7cd49fd6316bb8cbab5a2adc8b4c811bab
landtree <delandtree@gmail.com>
2025-02-17 09:29:18 -0800

    init libraries

---
+++ Blowers/src/light.h
@@ -0,0 +1,11 @@
+#ifdef small
+  CRGB ring[24];
+#elif large
+  CRGB ring[30];
+#endif
+
+
+void animationLight()
+{
+
+}
---
+++ Blowers/src/main.cpp
@@ -0,0 +1,15 @@
+#include <Arduino.h>
+#include <Timer.h>
+#include <Bounce2.h>
+#include <FastLED.h>
+#include "light.h"
+
+
+
+void setup() {
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+}
