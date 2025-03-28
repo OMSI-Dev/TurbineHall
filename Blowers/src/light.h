c6eab478ac6b242447d3f78bbd2c7cc566393f21
landtree <delandtree@gmail.com>
2025-03-28 14:47:42 -0700

    updated start btn function, changed ring animation

--- Blowers/src/light.h
+++ Blowers/src/light.h
@@ -12,8 +12,11 @@
   CRGB ledStrip[ledNum];
 #endif
 
-MoToTimer ringTimer;
+MoToTimer ledTimer,rampTimer;
 uint8_t i =0;
+uint16_t rampTime = 125, r = rampTime;
+
+bool rampLightSpeed = 1;
 
 #ifdef ring
   //break into sections
@@ -27,63 +30,100 @@ uint8_t i =0;
   CRGB* ledStrip1 = &ledStrip[0]; 
   CRGB* ledStrip2 = &ledStrip[(ledNum/2 * 1)]; 
 #endif
+
 uint16_t lightSpeedControl()
 {
     uint16_t tempRead = analogRead(pot);
     uint16_t tempSpeed = map(tempRead, 0,1024,100,20);
-
+    Serial.print("Pot: ");
+    Serial.println(tempRead);
     return tempSpeed;
+
+}
+
+uint16_t ringSpeed()
+{
+    uint16_t tempRead = analogRead(pot);
+    uint16_t tempPos = map(tempRead, 0,1024,1,25);
+    Serial.print("Pot: ");
+    Serial.println(tempRead);
+    tempPos = constrain(tempPos,1,25);
+    Serial.print("tempPos: ");
+    Serial.println(tempPos);
+    return tempPos;
 
 }
 
 #ifdef ring
   void animationLight()
   {
-      if(!ringTimer.running())
-      {
-          i++;
-          if(i==ledNum/4){i = 0;}
-
-          quad1[i] = CRGB::RoyalBlue;
-          quad2[i] = CRGB::RoyalBlue;
-          quad3[i] = CRGB::RoyalBlue;
-          quad4[i] = CRGB::RoyalBlue;
-
-          #if !defined(controlledSpeed)
-          ringTimer.setTime(50);
-          #else
-          uint8_t speed = lightSpeedControl();
-          ringTimer.setTime(speed);
-          #endif
-
-          fadeToBlackBy(ringLight,ledNum,55);
-      }
-      
-      FastLED.show();
+    //this is the highest position to put the led to.
+    uint8_t ledPos = ringSpeed();
+    //Set to current position
+    fill_gradient_RGB(ringLight,ledPos,CRGB::Green,CRGB::Red);
+    FastLED.show();
+    //clear and update again
+    FastLED.clear();
+    FastLED.show();
   }
 #endif
 
 #ifdef strip
   void animationLight()
   {
-        if(!ringTimer.running())
+      if(!rampLightSpeed)
+        {
+          if(!ledTimer.running())
+          {
+              
+              i++;
+              if(i==ledNum/2){i = 0;}
+
+              ledStrip1[i] = CRGB::OrangeRed;
+              ledStrip2[i] = CRGB::OrangeRed;
+
+              #if !defined(controlledSpeed)
+              ledTimer.setTime(50);
+              #else
+              uint8_t speed = lightSpeedControl();
+              ledTimer.setTime(speed);
+              #endif
+
+              fadeToBlackBy(ledStrip,ledNum,100);
+          }
+        }
+      if(rampLightSpeed)
+      {
+        if(!rampTimer.running())
         {
             i++;
             if(i==ledNum/2){i = 0;}
 
-            ledStrip1[i] = CRGB::RoyalBlue;
-            ledStrip2[i] = CRGB::RoyalBlue;
+            ledStrip1[i] = CRGB::OrangeRed;
+            ledStrip2[i] = CRGB::OrangeRed;
+            r--;
+            rampTimer.setTime(r);
 
-            #if !defined(controlledSpeed)
-            ringTimer.setTime(50);
-            #else
             uint8_t speed = lightSpeedControl();
-            ringTimer.setTime(speed);
-            #endif
 
-            fadeToBlackBy(ledStrip,ledNum,55);
+            Serial.print("R: ");
+            Serial.print(r);
+            Serial.print(" | ");
+            Serial.print("Speed: ");
+            Serial.println(speed);
+            
+            if(r==speed)
+            {
+              r=rampTime;
+              rampLightSpeed = 0;
+              Serial.println("Lights match Speed...");
+              Serial.print("R = ");
+              Serial.println(r);
+            }
+            fadeToBlackBy(ledStrip,ledNum,100);
         }
-        
+      }
+
         FastLED.show();
   }
 #endif
--- Blowers/src/pins.h
+++ Blowers/src/pins.h
@@ -1,50 +1,73 @@
-#define pot A0
-#define btnIn 7
-#define btnLight 10
-#define dataPin 4
+#define pot A1
+#define startBtnIn 7
+#define startBtnLight 10
+#define stopBtnIn 5
+#define stopBtnLight 9
+#define dataPin 1 //4
 #define motorPin 11
 #define motorEn 12
 
 //Comment out if strip or ring is being used
-#define strip
-//#define ring
+//#define strip
+#define ring
 
 //Comment out what ring is being used.
-#define large
-//#define small
+//#define large
+#define small
 
 //Comment out if motors just turn on
 #define controlledSpeed
 
-//used for RGBW Emulation
+//comment out if not using stop button
+#define hasStop
+
+//used for RGBW Emulation in fastLED
+//Do not change.
 Rgbw rgbw = Rgbw(
     kRGBWDefaultColorTemp,
     kRGBWExactColors,      // Mode
     W3                     // W-placement
 );
 
+//If the WS2812 gives an error, you can ignore it is correct and will compile
 typedef WS2812<dataPin, RGB> ControllerT;  // RGB mode must be RGB, no re-ordering allowed.
 static RGBWEmulatedController<ControllerT, GRB> rgbwEmu(rgbw);  // ordering goes here.
 
 Bounce2::Button startBtn = Bounce2::Button();
-Pulse btnPWM;
+Bounce2::Button stopBtn = Bounce2::Button();
+Pulse startBtnPWM,stopBtnPWM,heartBeat;
 
 void setPins()
 {
-    startBtn.attach(btnIn,INPUT_PULLUP);
+    startBtn.attach(startBtnIn,INPUT_PULLUP);
     startBtn.setPressedState(LOW);
     startBtn.interval(5);
-    btnPWM.attach(btnLight);
+    //attach to pulse objects
+    startBtnPWM.attach(startBtnLight);
+
+    //only attach if the stop button is being used
+    //Used on the shake tables but not on the blowers
+    #ifdef hasStop
+        stopBtn.attach(stopBtnIn,INPUT_PULLUP);
+        stopBtn.setPressedState(LOW);
+        stopBtn.interval(5);
+        stopBtnPWM.attach(stopBtnLight);
+    #endif
+
+    //used to debug
+    heartBeat.attach(13);
 
     pinMode(pot, INPUT);
-    pinMode(btnLight,OUTPUT);
+    pinMode(stopBtnLight,OUTPUT);
+    pinMode(startBtnLight,OUTPUT);
     pinMode(motorPin, OUTPUT);
     pinMode(motorEn, OUTPUT);
     pinMode(13,OUTPUT);
     //set output pins low/off
-    digitalWrite(btnLight, LOW);
+    digitalWrite(startBtnLight, LOW);
+    digitalWrite(stopBtnLight, LOW);
     digitalWrite(motorPin, LOW);
-    digitalWrite(motorEn,HIGH);
+    digitalWrite(motorEn,LOW);
 
 
 }
